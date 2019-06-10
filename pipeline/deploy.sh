814f2acab8a02bc0903fdc6eec5526d359faba2e
whls <ls273749432@126.com>
2019-06-10 23:11:19 +0800

    change deploy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -16,25 +16,3 @@ ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}:$
 sed -e "s;%ECR_IMAGE%;${ECR_IMAGE};g" ${ECS_TASK_DEFINITION_TEMPLATE} > ${ECS_TASK}
 aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://${ECS_TASK}
 
-result=$(aws ecs describe-services --cluster ${ECS_CLUSTER} --service ${SERVICE_NAME} | jq '.failures[0].reason')
-
-if [[ ${result} == "\"MISSING\"" ]]; then
-    aws ecs create-service --cluster ${ECS_CLUSTER} \
-                           --service-name ${SERVICE_NAME} \
-                           --cli-input-json file://service.json
-fi
-#OLD_TASK_ID=$(aws ecs list-tasks --cluster ${ECS_CLUSTER} --desired-status RUNNING --family ${TASK_FAMILY} | egrep "task" | tr "/" " " | tr "[" " " |  awk '{print $2}' | sed 's/"$//')
-#echo $OLD_TASK_ID
-TASK_REVISION=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq '.taskDefinition.revision')
-echo $TASK_REVISION
-DESIRED_COUNT=$(aws ecs describe-services --service ${SERVICE_NAME} --cluster ${ECS_CLUSTER} | jq '.services[0]|.desiredCount')
-echo $DESIRED_COUNT
-#if [[ ${DESIRED_COUNT} == "0" ]]; then
-#    DESIRED_COUNT="1"
-#fi
-#aws ecs stop-task --cluster ${ECS_CLUSTER} \
-#		  --task ${OLD_TASK_ID}
-#aws ecs update-service --cluster ${ECS_CLUSTER} \
-#                       --service ${SERVICE_NAME} \
-#                       --task-definition ${TASK_FAMILY}:${TASK_REVISION} \
-#                       --desired-count ${DESIRED_COUNT}
