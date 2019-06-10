8b9acaad13030df911d5aa35e1ea0aefcb2a4326
whls <ls273749432@126.com>
2019-06-10 23:43:10 +0800

    change deploy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -14,5 +14,10 @@ ECS_TASK="task-ecs.json"
 ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 # Register Task Definition
 sed -e "s;%ECR_IMAGE%;${ECR_IMAGE};g" ${ECS_TASK_DEFINITION_TEMPLATE} > ${ECS_TASK}
+TASK_REVISION=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq '.taskDefinition.revision')
+aws ecs deregister-task-definition --task-definition ${TASK_FAMILY}:${TASK_REVISION}
 aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://${ECS_TASK}
+aws ecs create-service --cluster ${ECS_CLUSTER} \
+                       --service-name ${SERVICE_NAME} \
+                       --cli-input-json file://service.json
 
